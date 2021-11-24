15845572897e5fe51431f1763f39486044ebde6e
Abir Mansour <abirmansour96@gmail.com>
2021-11-24 10:48:24 +0100

    Initial commit

---
+++ app/src/main/java/com/abir/affirmations/data/Datasource.kt
@@ -0,0 +1,21 @@
+package com.abir.affirmations.data
+
+import com.abir.affirmations.R
+import com.abir.affirmations.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
